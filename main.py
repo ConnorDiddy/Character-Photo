0754a64e7efdbf2ac1646cda3e8c8aedacbb1bda
ConnorDiddy <connordidericksen@gmail.com>
2022-05-20 14:17:29 -0600

    Added comments and cleaned up code

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import cv2
 
+# Initializes txt file
+with open("ascii_image.txt", "w") as f:
+        f.write('')
+
+# Import and resize photo
 img = cv2.imread('assets/mona-lisa.jpg', 0)
 img = cv2.resize(img, (100, 100))
 
 ASCII_CHARS = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.']
-with open("ascii_image.txt", "w") as f:
-        f.write('')
+
+# Loop through each pixel value and replace it with the ASCII character most closely matching the intensity
 for row in img:
     str = ''
     for pixel in row:
         index = (pixel // 25)
         str = str + (ASCII_CHARS[index])
-    with open("ascii_image.txt", "a") as f:
+    with open("ascii_image.txt", "a") as f: # Writes the row of characters into the next line of the txt file
         f.write('\n'+str)
 
 cv2.waitKey(0)
